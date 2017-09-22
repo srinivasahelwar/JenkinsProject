389efb9a7c1620a0b5e6b7eee9f6d5354b49f30a
Srinivasa R <Srinivasa R>
2017-09-22 07:35:09 +0530

    commited

---
+++ src/main/java/com/mulesoft/training/MuleComponent.java
@@ -0,0 +1,38 @@
+package com.mulesoft.training;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.mule.api.annotations.param.InboundHeaders;
+import org.mule.api.annotations.param.Payload;
+
+public class MuleComponent {
+
+	public Map<String,String> processMap(Map<String,String> input) {
+		input.put("processed by","processMap");
+		return input;
+	}
+
+	public Map<String,String> processArray(List<String> input) {
+		Map<String,String> output = new HashMap<String,String>();
+		output.put("message",input.get(0));
+		output.put("processedBy","processArray");
+		return output;
+	}
+	public Map<String,String> processString(String input) {
+		Map<String,String> output = new HashMap<String,String>();
+		output.put("message", input);
+		output.put("processedBy","processString");
+		return output;
+	}
+	
+	public Map<String,String> processAll(@Payload Object input,
+			@InboundHeaders("http.method") String method) {
+			Map<String,String> output = new HashMap<String,String>();
+			output.put("message", input.toString());
+			output.put("http.method", method);
+			output.put("processedBy","processAll");
+			return output;
+	}
+} 
